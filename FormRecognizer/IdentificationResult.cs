62c615dfc312c3b4c4f45043dbb5d0d6d6694910
Bogdan Bujdea <bogdan@thewindev.net>
2021-03-20 00:26:51 +0200

    Added Form Recognizer sample code

---
+++ FormRecognizer/IdentificationResult.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FormRecognizer
+{
+    public class IdentificationResult
+    {
+        public string Status { get; set; }
+        public DateTime CreatedDateTime { get; set; }
+        public DateTime LastUpdatedDateTime { get; set; }
+        public AnalyzeResult AnalyzeResult { get; set; }
+    }
+
+    public class AnalyzeResult
+    {
+        public string Version { get; set; }
+        public ReadResult[] ReadResults { get; set; }
+        public DocumentResult[] DocumentResults { get; set; }
+    }
+
+    public class ReadResult
+    {
+        public int Page { get; set; }
+        public float Angle { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public string Unit { get; set; }
+        public Line[] Lines { get; set; }
+    }
+
+    public class Line
+    {
+        public string Text { get; set; }
+        public int[] BoundingBox { get; set; }
+        public Word[] Words { get; set; }
+        public Appearance Appearance { get; set; }
+    }
+
+    public class Appearance
+    {
+        public Style Style { get; set; }
+    }
+
+    public class Style
+    {
+        public string Name { get; set; }
+        public float Confidence { get; set; }
+    }
+
+    public class Word
+    {
+        public string Text { get; set; }
+        public int[] BoundingBox { get; set; }
+        public float Confidence { get; set; }
+    }
+
+    public class DocumentResult
+    {
+        public string DocType { get; set; }
+        public float DocTypeConfidence { get; set; }
+        public int[] PageRange { get; set; }
+        public Fields Fields { get; set; }
+    }
+
+    public class Fields
+    {
+        public Field Address { get; set; }
+        public Field Country { get; set; }
+        public Field DateOfBirth { get; set; }
+        public Field DateOfExpiration { get; set; }
+        public Field DocumentNumber { get; set; }
+        public Field FirstName { get; set; }
+        public Field LastName { get; set; }
+        public Field Region { get; set; }
+        public Field Sex { get; set; }
+    }
+
+    public class Field
+    {
+        public string Type { get; set; }
+        public string ValueString { get; set; }
+        public string ValueDate { get; set; }
+        public string ValueCountry { get; set; }
+        public string Text { get; set; }
+        public int[] BoundingBox { get; set; }
+        public int Page { get; set; }
+        public float Confidence { get; set; }
+        public string[] Elements { get; set; }
+        public string ValueGender { get; set; }
+    }
+
+}
---
+++ FormRecognizer/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace FormRecognizer
+{
+    class Program
+    {
+        private const string ApiKey = "<your key here>";
+        private const string FormRecognizerEndpoint = "https://northeurope.api.cognitive.microsoft.com/";
+        private static HttpClient _client;
+
+        static async Task Main(string[] args)
+        {
+            var resultUrl = await StartAnalyzingImage();
+            IdentificationResult result;
+            do
+            {
+                await Task.Delay(1000); // wait 1 second before checking, otherwise the API will return 429
+                var json = await _client.GetStringAsync(resultUrl);
+                result = JsonConvert.DeserializeObject<IdentificationResult>(json);
+            } while (result?.Status != "succeeded");
+            
+            var documentResults = result.AnalyzeResult.DocumentResults;
+            if (documentResults != null && documentResults.Length != 0)
+            {
+                foreach (var documentResult in documentResults)
+                {
+                    Console.WriteLine($"First name: {documentResult.Fields.FirstName}, confidence: {documentResult.Fields.FirstName.Confidence}");
+                    Console.WriteLine($"Last name: {documentResult.Fields.FirstName}, confidence: {documentResult.Fields.LastName.Confidence}");
+                    Console.WriteLine($"Address: {documentResult.Fields.Address}, confidence: {documentResult.Fields.Address.Confidence}");
+                    Console.WriteLine($"Country: {documentResult.Fields.Country}, confidence: {documentResult.Fields.Country.Confidence}");
+                    Console.WriteLine($"DateOfBirth: {documentResult.Fields.DateOfBirth}, confidence: {documentResult.Fields.DateOfBirth.Confidence}");
+                    Console.WriteLine($"DateOfExpiration: {documentResult.Fields.DateOfExpiration}, confidence: {documentResult.Fields.DateOfExpiration.Confidence}");
+                    Console.WriteLine($"Region: {documentResult.Fields.Region}, confidence: {documentResult.Fields.Region.Confidence}");
+                    Console.WriteLine($"Sex: {documentResult.Fields.Sex}, confidence: {documentResult.Fields.Sex.Confidence}");
+                }
+            }
+        }
+
+        private static async Task<string> StartAnalyzingImage()
+        {
+            var queryString = HttpUtility.ParseQueryString(string.Empty);
+
+            _client = new HttpClient();
+
+            // set the key
+            _client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", ApiKey);
+
+            // set the endpoint
+
+            queryString["includeTextDetails"] = "true";
+
+            var uri = $"{FormRecognizerEndpoint}/formrecognizer/v2.1-preview.3/prebuilt/idDocument/analyze?{queryString}";
+
+            var image = System.IO.File.ReadAllBytes(@"d:\driving.jpg");
+            using var content = new ByteArrayContent(image);
+            content.Headers.ContentType = new MediaTypeHeaderValue("image/jpeg");
+            var response = await _client.PostAsync(uri, content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                // if the response is successful, it means that Form Recognizer started processing our image
+                // in order to get the result, we have to make GET requests to an URL
+                // that URL is provided in a header named "Operation-Location"
+                // below we're going to retrieve that URL and send it back to the caller of the function
+                var location = response.Headers.FirstOrDefault(h => h.Key == "Operation-Location");
+                return location.Value.FirstOrDefault();
+            }
+
+            return string.Empty;
+        }
+    }
+}
